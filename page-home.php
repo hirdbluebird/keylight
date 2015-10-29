cc9aabbb0a84bd1cdebf60d97cc4d2faa42c7e52
Arthur <arthur@netweightit.com>
2015-10-29 05:41:05 +0200

    js not working and not loading

--- page-home.php
+++ page-home.php
@@ -31,7 +31,7 @@ get_header(); ?>
 		                        <div class="third">
 		                            <div class="center-text">
 		                                <div class="knobcontainer">
-		                                    <input type="text" id="projectsknob" value="54" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="54" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
+		                                    <input type="text" id="projectsknob" value="<?php if ( get_field('number_project', get_the_ID()) != '' ) { the_field('number_project', get_the_ID()); } ?>" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="54" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
 		                                    <span class="knobtitle">Projects</span>
 		                                </div>
 		                            </div>
@@ -39,7 +39,7 @@ get_header(); ?>
 		                        <div class="third">
 		                            <div class="center-text">
 		                                <div class="knobcontainer">
-		                                    <input type="text" id="clientsknob" value="34" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="34" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
+		                                    <input type="text" id="clientsknob" value="<?php if ( get_field('number_clients', get_the_ID()) != '' ) { the_field('number_clients', get_the_ID()); } ?>" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="34" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
 		                                    <span class="knobtitle">Clients</span>
 		                                </div>
 		                            </div>
@@ -47,40 +47,56 @@ get_header(); ?>
 		                        <div class="third">
 		                            <div class="center-text">
 		                                <div class="knobcontainer">
-		                                    <input type="text" id="hoursknob" value="465" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="465" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
+		                                    <input type="text" id="hoursknob" value="<?php if ( get_field('number_hours', get_the_ID()) != '' ) { the_field('number_hours', get_the_ID()); } ?>" class="knob" data-readonly="true" data-width="290" data-height="290" data-min="0" data-max="465" data-skin="tron" data-thickness=".1" data-fgColor="#ffffff">
 		                                    <span class="knobtitle">Hours</span>
 		                                </div>
 		                            </div>
 		                        </div>
 		                    </div>
-		                    <p class="bigger width70 center mb40">No true fiasco ever began as a quest for mere adequacy. A motto of the British Special Air Force is: 'Those who risk, win.' A single green vine shoot is able to grow through cement.</p>
-		                    <a href="#work" class="scrollto btn borderonly">Work with us</a>
+		                    <p class="bigger width70 center mb40"><?php if ( get_field('benefits_text', get_the_ID()) != '' ) { the_field('benefits_text', get_the_ID()); } ?></p>
+		                    <a href="#work" class="scrollto btn borderonly"><?php if ( get_field('benefits_button', get_the_ID()) != '' ) { the_field('benefits_button', get_the_ID()); } ?></a>
 		                </div>
 		            </div> <!-- .content-container -->
 		        </div>
 
 
+		        <div id="work" class="section content-container white">
+		            <div class="wrapper">
+		                <h2 class="title center-text padding-small"><?php if ( get_field('work_title', get_the_ID()) != '' ) { the_field('work_title', get_the_ID()); } ?></h2>
+		            </div> <!-- .wrapper -->
+		            <div class="gallerycont">
+		                <!-- <div id="gallerynav" class="carousel-pagination gallery-nav inverse"></div> -->
+		                <div class="gallery clearfix">
+		                  <div data-id="id-11" class="item print"><a href="img/samples/portfolio/big/1.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-						<div class="entry-thumbnail">
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<?php endif; ?>
+		                  <div data-id="id-7" class="item web"><a href="img/samples/portfolio/big/2.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
 
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
+		                  <div data-id="id-8" class="item web"><a href="img/samples/portfolio/big/3.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-					</div><!-- .entry-content -->
+		                  <div data-id="id-9" class="item print"><a href="img/samples/portfolio/big/4.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description" data-options="width: 500" ><img src="http://placehold.it/480x360"></a></div>
+		                  
+		                  <div data-id="id-6" class="item web"><a href="img/samples/portfolio/big/5.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
 
-					<footer class="entry-meta">
-						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
-				</article><!-- #post -->
+		                  <div data-id="id-13" class="item print"><a href="img/samples/portfolio/big/7.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+
+		                  <div data-id="id-5" class="item web"><a href="img/samples/portfolio/big/8.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  
+		                  <div data-id="id-10" class="item print"><a href="img/samples/portfolio/big/9.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                </div>
+		                <a href="#" class="nav prev color2" id="portfolioprev">prev</a>
+		                <a href="#" class="nav next color2" id="portfolionext">next</a>
+		            </div>
+		            <div class="wrapper padding-tiny">
+		                <div class="center-text">
+		                    <p class="bigger width70 center mb40"><?php if ( get_field('work_text', get_the_ID()) != '' ) { the_field('work_text', get_the_ID()); } ?></p>
+		                        <a href="#contact" class="btn scrolltoslow mb50 color1"><?php if ( get_field('work_button', get_the_ID()) != '' ) { the_field('work_button', get_the_ID()); } ?></a>
+		                </div>
+		            </div>
+		        </div> <!-- .content-container -->
+
+
+				<?php the_content(); ?>
+			
 
 				<?php comments_template(); ?>
 			<?php endwhile; ?>
