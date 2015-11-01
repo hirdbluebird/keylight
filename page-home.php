3a45cf7f306ec346409431526d9d9ba4775dd75f
hirdbluebird <bluebirdyourdestiny@gmail.com>
2015-11-01 14:45:51 +0200

    slider WP

--- page-home.php
+++ page-home.php
@@ -67,21 +67,21 @@ get_header(); ?>
 		            <div class="gallerycont">
 		                <!-- <div id="gallerynav" class="carousel-pagination gallery-nav inverse"></div> -->
 		                <div class="gallery clearfix">
-		                  <div data-id="id-11" class="item print"><a href="img/samples/portfolio/big/1.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-11" class="item print"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 
-		                  <div data-id="id-7" class="item web"><a href="img/samples/portfolio/big/2.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-7" class="item web"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 
-		                  <div data-id="id-8" class="item web"><a href="img/samples/portfolio/big/3.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-8" class="item web"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 
-		                  <div data-id="id-9" class="item print"><a href="img/samples/portfolio/big/4.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description" data-options="width: 500" ><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-9" class="item print"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>" data-options="width: 500" ><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 		                  
-		                  <div data-id="id-6" class="item web"><a href="img/samples/portfolio/big/5.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-6" class="item web"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 
-		                  <div data-id="id-13" class="item print"><a href="img/samples/portfolio/big/7.jpg" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-13" class="item print"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 
-		                  <div data-id="id-5" class="item web"><a href="img/samples/portfolio/big/8.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-5" class="item web"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 		                  
-		                  <div data-id="id-10" class="item print"><a href="img/samples/portfolio/big/9.png" data-lightbox-gallery="gallery1" data-title="Item title" data-caption="Item Description"><img src="http://placehold.it/480x360"></a></div>
+		                  <div data-id="id-10" class="item print"><a href="<?php if ( get_field('work_image', get_the_ID()) != '' ) { the_field('work_image', get_the_ID()); } ?>" data-lightbox-gallery="gallery1" data-title="<?php if ( get_field('work_item_title', get_the_ID()) != '' ) { the_field('work_item_title', get_the_ID()); } ?>" data-caption="<?php if ( get_field('work_item_description', get_the_ID()) != '' ) { the_field('work_item_description', get_the_ID()); } ?>"><img src="<?php if ( get_field('work_thumbnail', get_the_ID()) != '' ) { the_field('work_thumbnail', get_the_ID()); } ?>"></a></div>
 		                </div>
 		                <a href="#" class="nav prev color2" id="portfolioprev">prev</a>
 		                <a href="#" class="nav next color2" id="portfolionext">next</a>
@@ -94,6 +94,42 @@ get_header(); ?>
 		            </div>
 		        </div> <!-- .content-container -->
 
+		        <div id="quote" class="container-group" style="background-image: url(img/samples/background-2.jpg)">
+		            <div class="content-container">
+		                <div class="wrapper padding-large center-text">
+		                    <p class="quote center width70"><?php if ( get_field('quote_1', get_the_ID()) != '' ) { the_field('quote_1', get_the_ID()); } ?></p>
+		                    <a href="#pricing" class="scrollto btn color1"><?php if ( get_field('quote_1', get_the_ID()) != '' ) { the_field('quote_1', get_the_ID()); } ?></a>
+		                </div> <!-- .wrapper -->
+		            </div> <!-- .content-container -->
+
+		            <div id="us" class="section content-container color2">
+		                <div class="wrapper padding">
+		                    <div id="tabs" class="tabs width70">
+		                        <ul>
+		                            <li><a href="#tabs-1" title="">Our motto</a></li>
+		                            <li><a href="#tabs-2" title="">The story</a></li>
+		                            <li><a href="#tabs-3" title="">The office</a></li>
+		                        </ul>
+
+		                        <div id="tabs_container" class="clearfix">
+		                            <div id="tabs-1">
+		                                    <p class="bigger">No true fiasco ever began as a quest for mere adequacy. A motto of the British Special Air Force is: 'Those who risk, win.' A single green vine shoot is able to grow through cement. The Pacific Northwestern salmon beats itself bloody on it's quest to travel hundreds of miles upstream against the current, with a single purpose, sex of course, but also... life</p>
+		                                    <hr class="width30 center">
+		                                    <p class="bigger"><a href="#" class="dashed bold"></a></p>
+		                            </div>
+		                            <div id="tabs-2">
+		                                    <p class="bigger">Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor.</p>
+		                        
+		                            </div>
+		                            <div id="tabs-3">
+		                                    <p class="bigger">Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem.</p><p> Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales.</p>
+		                            </div>
+		                        </div><!--End tabs container-->
+		                    </div><!--End tabs-->
+
+		                </div> <!-- .wrapper -->
+		            </div> <!-- .content-container -->
+		        </div>
 
 				<?php the_content(); ?>
 			
